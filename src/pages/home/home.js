00794ca60c700441d7c568719029773ba3ea4dcf
Noxiium <johansf98@hotmail.com>
2024-09-23 22:04:19 +0200

    Created home and about page

---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+
+/**
+ * Renders the home page.
+ * @returns {JSX.Element} the home component.
+ */
+export function Home() {
+    return (
+        <div className="home-page">
+            <h1>Home Page of My-Movie-Box</h1>
+        </div>
+    );
+}
